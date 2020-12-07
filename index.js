2f7c044658f6bf2f732f0648c67c36e5e8683ae7
TheGoldenWaffle <47334268+TheGoldenWaffle@users.noreply.github.com>
2020-12-07 16:14:13 -0500

    hjkvsdhkcgjx

--- index.js
+++ index.js
@@ -1,133 +1,194 @@
 
+
+
 var target = null;
 const teamletters = document.querySelector("#tletters");
-teamletters.style.display = "flex";
+const mainletters = document.querySelector("#main-letters")
+const vid = document.getElementById("v")
+if (teamletters != null) {
+  teamletters.style.display = "flex";
+  vid.style.display = "none";
+} else {
+  mainletters.style.display = "flex"
+}
+
+
 
 const scroll = new LocomotiveScroll({
-    repeat: true
+  repeat: true
 
 });
-
+var mainPage = false;
+if (document.querySelector(".bg-tile-1") == null) {
+  mainPage = true
+}
 // Setup isScrolling variable
 var isScrolling;
 
 // Listen for scroll events
-window.addEventListener('scroll', function ( event ) {
-    if(document.querySelector(".bg-tile-1").getBoundingClientRect().top <= window.outerHeight/3 && teamletters.style.display == "flex") {
-        teamletters.style.display = "none";
-    } else if(document.querySelector(".bg-tile-1").getBoundingClientRect().top > window.outerHeight/3 && teamletters.style.display == "none") {
-        teamletters.style.display = "flex";
+window.addEventListener('scroll', function (event) {
+  if (!mainPage) {
+    if (document.querySelector(".bg-tile-1").getBoundingClientRect().top <= window.outerHeight / 3 && teamletters.style.display == "flex") {
+      teamletters.style.display = "none";
+    } else if (document.querySelector(".bg-tile-1").getBoundingClientRect().top > window.outerHeight / 3 && teamletters.style.display == "none") {
+      teamletters.style.display = "flex";
     }
-	// Clear our timeout throughout the scroll
-	window.clearTimeout( isScrolling );
 
-	// Set a timeout to run after scrolling ends
-	isScrolling = setTimeout(function() {
 
-        //collect the tops of each tile section on the page
-        var tiletops = [
-            document.querySelector(".bg-tile-0").getBoundingClientRect().top,
-            document.querySelector(".bg-tile-1").getBoundingClientRect().top,
-            document.querySelector(".bg-tile-2").getBoundingClientRect().top,
-            document.querySelector(".bg-tile-3").getBoundingClientRect().top,
-            document.querySelector(".bg-tile-4").getBoundingClientRect().top]
+    if (document.querySelector(".bg-tile-2").getBoundingClientRect().top < 0 && vid.style.display == "none") {
+      vid.style.display = "flex"
+    }
+    else if (document.querySelector(".bg-tile-2").getBoundingClientRect().top > 0 && vid.style.display == "flex") {
+      vid.style.display = "none"
+    }
+    // Clear our timeout throughout the scroll
+    window.clearTimeout(isScrolling);
 
-        // reset our target to null
-        target = null;
-        // loop through tiled sections
-        for(var i = 0; i<tiletops.length; i++) {
-            
-            // these if and else if make it know if a tile is within 1/8 of the viewport 
-            if(tiletops[i] >= 0 && tiletops[i] < window.outerHeight/8) {
-                item = ".bg-tile-" + i;
-                target = document.querySelector(item);
-            } else if(tiletops[i] <= 0 && tiletops[i] > window.outerHeight/-8) {
-                if(i != 4) {
-                    item = ".bg-tile-" + i;
-                    target = document.querySelector(item);
-                }
-                
-            }
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function () {
+
+      //collect the tops of each tile section on the page
+      var tiletops = [
+        document.querySelector(".bg-tile-0").getBoundingClientRect().top,
+        document.querySelector(".bg-tile-1").getBoundingClientRect().top,
+        document.querySelector(".bg-tile-2").getBoundingClientRect().top,
+        document.querySelector(".bg-tile-3").getBoundingClientRect().top,
+        document.querySelector(".bg-tile-4").getBoundingClientRect().top]
+
+      // reset our target to null
+      target = null;
+      // loop through tiled sections
+      for (var i = 0; i < tiletops.length; i++) {
+
+        // these if and else if make it know if a tile is within 1/8 of the viewport 
+        if (tiletops[i] >= 0 && tiletops[i] < window.outerHeight / 8) {
+          item = ".bg-tile-" + i;
+          target = document.querySelector(item);
+        } else if (tiletops[i] <= 0 && tiletops[i] > window.outerHeight / -8) {
+          if (i != 4) {
+            item = ".bg-tile-" + i;
+            target = document.querySelector(item);
+          }
+
         }
-        if(target != null) {
-            scroll.scrollTo(target);
+      }
+      if (target != null) {
+        scroll.scrollTo(target);
+      }
+
+
+
+    }, 40);
+
+  } else {
+    if (document.querySelector(".main-tile-1").getBoundingClientRect().top <= window.outerHeight / 6 && mainletters.style.display == "flex") {
+      mainletters.style.display = "none";
+    } else if (document.querySelector(".main-tile-1").getBoundingClientRect().top > window.outerHeight / 6 && mainletters.style.display == "none") {
+      mainletters.style.display = "flex";
+    }
+
+    // Clear our timeout throughout the scroll
+    window.clearTimeout(isScrolling);
+
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function () {
+
+      //collect the tops of each tile section on the page
+      var tiletops = [
+        document.querySelector(".main-tile-0").getBoundingClientRect().top,
+        document.querySelector(".main-tile-1").getBoundingClientRect().top,
+        document.querySelector(".main-tile-2").getBoundingClientRect().top,
+        document.querySelector(".main-tile-3").getBoundingClientRect().top,
+        document.querySelector(".main-tile-4").getBoundingClientRect().top]
+
+      // reset our target to null
+      target = null;
+      // loop through tiled sections
+      for (var i = 0; i < tiletops.length; i++) {
+
+        // these if and else if make it know if a tile is within 1/8 of the viewport 
+        if (tiletops[i] >= 0 && tiletops[i] < window.outerHeight / 8) {
+          item = ".main-tile-" + i;
+          target = document.querySelector(item);
+        } else if (tiletops[i] <= 0 && tiletops[i] > window.outerHeight / -8) {
+          if (i != 4) {
+            item = ".main-tile-" + i;
+            target = document.querySelector(item);
+          }
+
         }
+      }
+      if (target != null) {
+        scroll.scrollTo(target);
+      }
 
-		
 
-	}, 40);
+
+    }, 40);
+  }
 
 }, false);
+if (!mainPage) {
+  var slideIndex = 1;
 
-var slideIndex = 1;
+  var myTimer;
 
-var myTimer;
+  var slideshowContainer;
 
-var slideshowContainer;
-
-window.addEventListener("load",function() {
+  window.addEventListener("load", function () {
     showSlides(slideIndex);
-    myTimer = setInterval(function(){plusSlides(1)}, 4000);
-  
-    
-    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
-  
-    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
-  
-    slideshowContainer.addEventListener('mouseenter', pause)
-    slideshowContainer.addEventListener('mouseleave', resume)
-})
+    myTimer = setInterval(function () { plusSlides(1) }, 4000);
+  })
 
-// NEXT AND PREVIOUS CONTROL
-function plusSlides(n){
-  clearInterval(myTimer);
-  if (n < 0){
-    showSlides(slideIndex -= 1);
-  } else {
-   showSlides(slideIndex += 1); 
+  // NEXT AND PREVIOUS CONTROL
+  function plusSlides(n) {
+    clearInterval(myTimer);
+    if (n < 0) {
+      showSlides(slideIndex -= 1);
+    } else {
+      showSlides(slideIndex += 1);
+    }
+
+    //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
+    if (n === -1) {
+      myTimer = setInterval(function () { plusSlides(n + 2) }, 4000);
+    } else {
+      myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+    }
   }
-  
-  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-  
-  if (n === -1){
-    myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
-  } else {
-    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+
+  //Controls the current slide and resets interval if needed
+  function currentSlide(n) {
+    clearInterval(myTimer);
+    myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
+    showSlides(slideIndex = n);
   }
-}
 
-//Controls the current slide and resets interval if needed
-function currentSlide(n){
-  clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n){
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
+    }
+    for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  pause = () => {
+    clearInterval(myTimer);
+  }
+
+  resume = () => {
+    clearInterval(myTimer);
+    myTimer = setInterval(function () { plusSlides(slideIndex) }, 4000);
+  }
+
 }
-
-pause = () => {
-  clearInterval(myTimer);
-}
-
-resume = () =>{
-  clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
-}
-
-
 
